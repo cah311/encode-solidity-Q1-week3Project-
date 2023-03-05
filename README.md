dc1c8f7d0ebf10a9270abb2568a3d81f33c276b2
Carson <c_halter@live.ca>
2023-03-05 07:36:17 -0600

    add deployment for ballot contract

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 - Develop and run scripts for “TokenizedBallot.sol” within your group to give voting tokens, delegating voting power, casting votes, checking vote power and querying results
 - Write a report with each function execution and the transaction hash, if successful, or the revert reason, if failed
 - Share your code in a github repo in the submission form
+
+Deployment contract command:
+
+```
+// for hardhat blockchain
+yarn run ts-node --files ./scripts/DeploymentHH.ts <arg1> <arg2> ...
+```
--- scripts/DeploymentHH.ts
+++ scripts/DeploymentHH.ts
@@ -1,26 +1,50 @@
 import { ethers } from "hardhat";
-import { MyToken__factory } from "../typechain-types";
+import { MyToken__factory, TokenizedBallot__factory } from "../typechain-types";
+
+// converts string argumetns to bytes
+function convertStringArrayToBytes32(array: string[]) {
+  const bytes32Array = [];
+  for (let index = 0; index < array.length; index++) {
+    bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
+  }
+  return bytes32Array;
+}
 
 async function main() {
   // connect to hardhat wallets
-
   const [deployer, account1, account2, account3] = await ethers.getSigners();
-  const contractFactory = new MyToken__factory(deployer);
-  const contract = await contractFactory.deploy();
-  const deployTxReciept = await contract.deployTransaction.wait();
+  const tokenContractFactory = new MyToken__factory(deployer);
+  const tokenContract = await tokenContractFactory.deploy();
+  const deployTxReciept = await tokenContract.deployTransaction.wait();
   console.log(
-    `The contract was deplyed at ${contract.address} at the block number ${deployTxReciept.blockNumber}`
+    `The contract was deplyed at ${tokenContract.address} at the block number ${deployTxReciept.blockNumber}`
   );
 
   // set arguments for proposal
   const args = process.argv;
-  const proposals = args.slice(1);
+  const proposals = args.slice(2);
   if (proposals.length <= 0) throw new Error("Missing argument: proposals");
-  console.log("Deploying Ballot contract");
   console.log("Proposals: ");
   proposals.forEach((element, index) => {
     console.log(`Proposal N. ${index + 1}: ${element}`);
   });
+
+  // set block number
+  const currentBlock = await ethers.provider.getBlock("latest");
+  console.log(`We are currently at block ${currentBlock.number}`);
+
+  // create ballot contract
+  const ballotContractFactory = new TokenizedBallot__factory(deployer);
+  console.log("Deploying ballot contract...");
+  const ballotContract = await ballotContractFactory.deploy(
+    convertStringArrayToBytes32(proposals),
+    tokenContract.address,
+    currentBlock.number
+  );
+  const ballotContractTxReceipt = await ballotContract.deployTransaction.wait();
+  console.log(
+    `The ballot contract was deployed at ${ballotContract.address} in block number ${ballotContractTxReceipt.blockNumber}`
+  );
 }
 
 main().catch((error) => {
